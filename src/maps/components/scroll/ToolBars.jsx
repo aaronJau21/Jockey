0af22bf9478fa470effa02cb05944cb8821f4b5c
aaronJau21 <aaronjau21@gmail.com>
2023-12-08 09:08:13 -0500

    cambios

--- src/maps/components/scroll/ToolBars.jsx
+++ src/maps/components/scroll/ToolBars.jsx
@@ -24,75 +24,84 @@ const ToolBars = ({ setStateLimpiar, setOptions }) => {
   }, [limpiar, showOptions]);
 
   return (
-    <div
-      className="mt-5 2xl:mt-0 flex flex-col "
-      style={{ position: "fixed", right: 30, top: 300 }}
-    >
-      <div className="tooltip">
-        <button
-          className="bg-yellow-400 p-2 rounded-md hover:bg-gray-300 shadow-gray-500 shadow-md"
-          onClick={() => setLimpiar(!limpiar)}
-        >
-          <img
-            src="http://13.59.46.236/img/limpiar.svg"
-            alt="Limpiar Pantalla"
-            className="w-6"
-          />
-          <span className="tooltiptext">Eliminar Polìgonos</span>
-        </button>
-      </div>
-      {limpiar && "Activo"}
+    <>
+      <div
+        className="mt-5 2xl:mt-0 flex flex-col "
+        style={{ position: "fixed", right: 30, top: 300 }}
+      >
+        <div className="tooltip">
+          <button
+            className="bg-yellow-400 p-2 rounded-md hover:bg-gray-300 shadow-gray-500 shadow-md"
+            onClick={() => setLimpiar(!limpiar)}
+          >
+            <img
+              src="http://13.59.46.236/img/limpiar.svg"
+              alt="Limpiar Pantalla"
+              className="w-6"
+            />
+            <span className="tooltiptext">Eliminar Polìgonos</span>
+          </button>
+        </div>
+        {limpiar && "Activo"}
 
-      <div className="tooltip">
-        <button
-          className="bg-sky-400 p-2 rounded-md mt-3 hover:bg-gray-300 shadow-gray-500 shadow-md"
-          onClick={() => setShowOptions(!showOptions)}
-        >
-          <img
-            src="http://13.59.46.236/img/grupo.svg"
-            alt="grupo"
-            className="w-6"
-          />
-        </button>
-        <span className="tooltiptext">Guardar Polígonos</span>
-      </div>
-      <div className="tooltip">
-        <button className="bg-yellow-400 p-2 rounded-md mt-3 shadow-gray-500 shadow-md">
-          <img
-            src="http://13.59.46.236/img/union.svg"
-            alt="Union"
-            className="w-6"
-          />
-          <span className="tooltiptext">Unión de poligonos</span>
-        </button>
-      </div>
+        <div className="tooltip">
+          <button
+            className="bg-sky-400 p-2 rounded-md mt-3 hover:bg-gray-300 shadow-gray-500 shadow-md"
+            onClick={() => setShowOptions(!showOptions)}
+          >
+            <img
+              src="http://13.59.46.236/img/grupo.svg"
+              alt="grupo"
+              className="w-6"
+            />
+          </button>
+          <span className="tooltiptext">Guardar Polígonos</span>
+        </div>
+        <div className="tooltip">
+          <button className="bg-yellow-400 p-2 rounded-md mt-3 shadow-gray-500 shadow-md">
+            <img
+              src="http://13.59.46.236/img/union.svg"
+              alt="Union"
+              className="w-6"
+            />
+            <span className="tooltiptext">Unión de poligonos</span>
+          </button>
+        </div>
 
-      <div className="tooltip">
-        <button
-          className="bg-yellow-400 p-2 rounded-md mt-3 shadow-gray-500 shadow-md"
-          onClick={openModalList}
-        >
-          <img
-            src="http://13.59.46.236/img/listado1.svg"
-            alt="Union"
-            className="w-6"
-          />
-          <span className="tooltiptext">Listado de Propuestas</span>
-        </button>
+        <div className="tooltip">
+          <button
+            className="bg-yellow-400 p-2 rounded-md mt-3 shadow-gray-500 shadow-md"
+            onClick={openModalList}
+          >
+            <img
+              src="http://13.59.46.236/img/listado1.svg"
+              alt="Union"
+              className="w-6"
+            />
+            <span className="tooltiptext">Listado de Propuestas</span>
+          </button>
+        </div>
+        {showList && <Listado closeModalList={closeModalList} />}
       </div>
-      {showList && <Listado closeModalList={closeModalList} />}
-
       <div className="tooltip">
         <button
           className="bg-yellow-400 p-2 rounded-md mt-3 shadow-gray-500 shadow-md"
           onClick={() => setInfoLocal(!infoLocal)}
         >
-          {infoLocal ? 'Haga Click en un local' : <i className="fa-solid fa-circle-info text-2xl"></i>}
+          {infoLocal ?
+            <>
+              <p>Haga Click en un local</p>
+              <span className="tooltiptextsecond">Muestra Información</span>
+            </> :
+            <>
+              <i className="fa-solid fa-circle-info text-2xl"></i>
+              <span className="tooltiptext">Muestra Información</span>
+            </>}
 
-          <span className="tooltiptext">Muestra Información</span>
+
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
